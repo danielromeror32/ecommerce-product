7e5306347101959cb51d0faa033d85da23d30d51
danielromeror32 <daniel321789@gmail.com>
2023-06-06 15:36:03 -0600

    functions to JavaScript

--- app.js
+++ app.js
@@ -3,3 +3,25 @@
 
 // divElement.remove();
 
+const btnMinus = document.querySelector(".btn-minus");
+const btnPlus = document.querySelector(".btn-plus");
+const count = document.querySelector("p.number-count");
+
+btnMinus.addEventListener("click", function(){
+    counts(-1)
+});
+btnPlus.addEventListener("click", function(){
+    counts(1)
+});
+
+function counts(operator) {
+    const value = parseInt(count.textContent)
+    if (value > 0 ){
+        count.textContent = value + operator;
+    }else{
+        count.textContent = value + 1;
+    }
+
+
+    
+};
