4e389bc27200ad7246f426b2a20917ba9329e068
danielromeror32 <daniel321789@gmail.com>
2023-06-06 21:02:01 -0600

    adding container cart

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-// var divElement = document.getElementById("menu");
-
-
-// divElement.remove();
 
 const btnMinus = document.querySelector(".btn-minus");
 const btnPlus = document.querySelector(".btn-plus");
 const count = document.querySelector("p.number-count");
+const animatedDiv = document.querySelector(".div-float");
+const toggleButton = document.querySelector(".container-car");
 
 btnMinus.addEventListener("click", function(){
     counts(-1)
@@ -21,7 +19,10 @@ function counts(operator) {
     }else{
         count.textContent = value + 1;
     }
-
-
-    
 };
+
+
+toggleButton.addEventListener("click", function() {
+    animatedDiv.classList.toggle("hidden");
+    
+});
